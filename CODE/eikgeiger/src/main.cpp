3f11f1b3458707a701cacf15898144d3ddc67dfa
Fredrik N Beilegaard <fredrik.kleven@outlook.com>
2022-12-10 23:06:43 +0100

    Feat: Got rid of pid

--- CODE/eikgeiger/src/main.cpp
+++ CODE/eikgeiger/src/main.cpp
@@ -3,6 +3,7 @@
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include "ESPAsyncWebServer.h"
+#include <EEPROM.h>
 
 #define BUF_VOLT 100
 #define BUF_VOLT_LONG 200
@@ -21,21 +22,7 @@ unsigned long buf_click_hour[BUF_CLICK]  = {0};
 int           buf_click_hour_index       =  0;
 float         buf_click_hour_avg         =  0.0;
 
-bool ss_flag = false;
-bool enable_psup = true;
-
-unsigned long runtime_ = 0;
-unsigned long runtime = 0;
-
-//PID variables:
-double I=0, I_=0, p=0, p_=0, setpoint=0, input=0, dp=0, output=0;
-double Kp = 1., Ki = 2.0, Kd = 0.00;
-double pid = 0;
-
-//Filter:
-float x[] = {0,0,0};
-float y[] = {0,0,0};
-int raw_read = 0;
+uint setpoint=340;
 
 uint32_t Freq;
 int LED_0= 0; 
@@ -67,19 +54,30 @@ unsigned long now = last+1;
 
 unsigned long last_2 = now+1;
 unsigned long now_2 = last_2+1;
-
-unsigned long last_3 = micros();
-unsigned long now_3 = last_3+1;
-
-unsigned long last_4 = micros();
-unsigned long now_4 = last_4+1;
-
 int BUCK_PWM = 0;
 int CLICKER_PWM = 12;
 
 DNSServer dnsServer;
 AsyncWebServer server(80);
 
+
+struct data_layout
+{
+  char id[16];
+  char ssid[128];
+  char pwd[128];
+  char ip[128];
+  bool first_run;
+  int number1;
+  byte number2;
+  byte number3;
+  byte number4;
+  byte number5;
+
+};
+
+data_layout persistant_data;
+data_layout temp_data;
 
 void IRAM_ATTR ISR() {
     ticks_old = ticks;
@@ -157,10 +155,12 @@ public:
   void handleRequest(AsyncWebServerRequest *request) {
     AsyncResponseStream *response = request->beginResponseStream("text/html");
     response->print("<!DOCTYPE html><html><head><title>EIKGEIGER WIFI LOGIN</title></head><body>");
-    response->printf("<p>Geigertelleren er aktiv, den er innstilt til %.2f volt</p>",voltdivider(adc2volt(input),50000,100));
+    response->print( "<h1>EIKGEIGER</h1>");
+    response->printf("<p>Geigertelleren er aktiv, den er innstilt til %i </p>",setpoint);
     response->print( "<p>Hvis du vil endre spenning brukes seriellport gjennom PC.</p>");
+    response->print( "<p>Bruk kommando s### , der ### er et heltall mellom 0-2000.</p>");
     response->printf("<p>Gjennomsnittlig CPM siste minutt er  %.2f klikk per minutt</p>", array_minmax_avg(buf_click, BUF_CLICK));
-    response->print( "<p>Vennligst velg hvilket wifi du vil koble deg til, og skriv inn passord passord.</p>");
+    response->print( "<p>Vennligst velg hvilket wifi du vil koble deg til, og skriv inn passord</p>");
     response->printf("<p>You were trying to reach: http://%s%s</p>", request->host().c_str(), request->url().c_str());
     response->printf("<p>Try opening <a href='http://%s'>this link</a> instead</p>", WiFi.softAPIP().toString().c_str());
     response->print( "</body></html>");
@@ -170,8 +170,6 @@ public:
 
 void setup() {
   Serial.begin(115200);
-
-  setpoint = revsetpoint(370);
 
   analogSetWidth(11);
   analogSetAttenuation(ADC_11db);
@@ -200,7 +198,7 @@ void setup() {
   ledcSetup(CLICKER_PWM, 6000, 8);
   
   //ledcWrite(CLICKER_PWM, 0);
-  ledcWrite(BUCK_PWM, pwm_int);
+  ledcWrite(BUCK_PWM, setpoint);
 
   //Deep sleep
   esp_sleep_enable_timer_wakeup(5 * 1e6);
@@ -213,7 +211,6 @@ void setup() {
 }
 
 void loop() {
-  delay(1);
   unsigned long start = micros();
   
   //Clicker logic
@@ -222,7 +219,7 @@ void loop() {
     duration = millis();
     //cpm = float(60e6/ticks_dt);
     click_old=clicks;
-    ledcWrite(CLICKER_PWM, 10);
+    ledcWrite(CLICKER_PWM, 80);
     digitalWrite(LED_0, LOW);
     digitalWrite(LED_1, LOW);
     digitalWrite(LED_2, LOW);
@@ -247,50 +244,15 @@ void loop() {
     inchar = Serial.read();
 
     switch (inchar){
-        case 'p':
+        case 'v':
           serial_input= Serial.parseFloat();
-          Kp=serial_input;
-          break;
-        case 'i':
-          serial_input= Serial.parseFloat();
-          I=0;
-          Ki=serial_input;
-          break;
-        case 'd':
-          serial_input= Serial.parseFloat();
-          Kd=serial_input;
+          setpoint=serial_input;
+          ledcWrite(BUCK_PWM, setpoint);
           break;
         case 's':
-          serial_input= Serial.parseFloat();
-          setpoint=revsetpoint(serial_input);
-          break;
-        case 'r':
-          Ki=0;
-          Kd=0;
-          Kp=0;
-          setpoint=400.0;
-          break;
-        case 'k':
-          Serial.print("Kp:");
-          Serial.print(Kp);
-          Serial.print(" Ki:");
-          Serial.print(Ki);
-          Serial.print(" Kd:");
-          Serial.print(Kd);
-          Serial.print(" setpoint:");
-          Serial.print(setpoint);
-          Serial.println();
-          break;
-        case 'o':
-          output = 0;
-          I=0;
-          ledcWrite(BUCK_PWM,0);
-          enable_psup = Serial.parseInt();
-          break;
+
+        break;
     }
-
-
-
   }
   //COUNTER LOOP
   now = millis();
@@ -310,21 +272,17 @@ void loop() {
   }
   
   //SERIAL LOOP
-  now_4 = millis();
-  if(now_4-last_4 > 1000 ){ //do this every second
-    last_4 = now_4;
+  now_2 = millis();
+  if(now_2-last_2 > 1000 ){ //do this every 100ms
+    last_2 = now_2;
 
     buf_volt_avg = array_calculate_avg(buf_volt, BUF_VOLT);
     float buf_cpm_avg_min = array_minmax_avg(buf_click, BUF_CLICK);
     float buf_cpm_avg_hour = array_minmax_avg(buf_click_hour, BUF_CLICK)/60.;
     Serial.print("raw_volt:");
-    Serial.print( voltdivider(adc2volt(analogRead(REFpin)),50000,100));
-    Serial.print(" volt:");
-    Serial.print( voltdivider(adc2volt(input),50000,100));
+    Serial.print( analogRead(REFpin));
     Serial.print(" set_volt:");
-    Serial.print( voltdivider(adc2volt(setpoint),50000,100));
-    Serial.print(" output");
-    Serial.print(output);
+    Serial.print( setpoint);
     Serial.print(" cpmM:");
     Serial.print(buf_cpm_avg_min);
     Serial.print(" cpmH:");
@@ -333,78 +291,6 @@ void loop() {
     Serial.print( float(60/float(ticks_dt/1e6)));
     Serial.println();
   }
-  
-  //ADC LOOP
-  now_2 = micros();
-  if(now_2-last_2 > 1000 && enable_psup){ //Do this every 10 000 us
-    last_2 = now_2;
-    raw_read = analogRead(REFpin);
-    
-    x[0] = raw_read;
-
-    float b[] = {0.06396438485558797, 0.12792876971117606, 0.06396438485558781};
-    float a[] = {1.1682606671932643, -0.4241182066156163};
-
-    y[0] = a[0]*y[1] + a[1]*y[2] +
-           b[0]*x[0] + b[1]*x[1] + b[2]*x[2];
-    
-    for(int i = 1; i >= 0; i--){
-      x[i+1] = x[i]; // store xi
-      y[i+1] = y[i]; // store yi
-    }
-
-    if (BUF_VOLT == buf_volt_index){
-      buf_volt_index = 0;
-    }
-
-    buf_volt[buf_volt_index++] = y[0];
-  }
-  
-  //PID LOOP
-  now_3 = micros();
-  if(now_3-last_3 > 100000 && enable_psup){ //Do this every 10 ms
-
-    double dt = (now_3-last_3)/1e6;
-    last_3 = now_3;
-
-    input = array_calculate_avg(buf_volt, BUF_VOLT);
-    p_ = p;
-    
-    p=setpoint-input;
-
-    dp = double((p-p_)/dt);
-
-    I_= I;
-    I = I + dt*((p+p_)/2.);
-
-    if ( I < 0){
-      I = I_;
-    }
-
-    if ( abs(I)*Ki > 3000){
-      I = I_;
-    }
-    double output_ = output;
-    output = p*Kp+I*Ki+dp*Kd;
-
-    if ( output < 0){
-      output = 0;
-    }
-
-    if ( output > 1500){
-      output = 1500;
-      ss_flag = true;
-    }
-
-    else{
-      ss_flag = false;
-    }
-
-    ledcWrite(BUCK_PWM, int(output));
-  }
-  
-  runtime_ = runtime;
-  runtime =  micros()-start;
 
   dnsServer.processNextRequest();
 }
